244e580b6be7b7164b9a5973b1d8c827b30acb78
Mats Ribaucourt <mats-ribaucourt@hotmail.com>
2023-03-07 16:01:47 +0700

    Local Notifications

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -4,6 +4,7 @@ import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { Platform } from "react-native";
 import * as Animatable from "react-native-animatable";
+import * as Notifications from"expo-notifications";
 
 const ReservationScreen = () => {
     const [campers, setCampers] = useState(1);
@@ -37,7 +38,7 @@ const ReservationScreen = () => {
                 {
                     text: "OK",
                     onPress: () => {
-                        console.log("OK");
+                        presentLocalNotification(date.toLocaleDateString("en-US"));
                         resetForm();
                     }
                 }
@@ -50,6 +51,34 @@ const ReservationScreen = () => {
         setHikeIn(false);
         setDate(new Date());
         setShowCalendar(false);
+    }
+
+    const presentLocalNotification = async (reservationDate) => {
+        const sendNotification = () => {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true
+                })
+            })
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: "Your campsite reservation search",
+                    body: `Search for ${reservationDate} requested`
+                },
+                trigger: null
+            })
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     }
 
     return (
