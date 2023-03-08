9d16c1dcd10662542722be7986a4f2e4e4979c4b
Mats Ribaucourt <mats-ribaucourt@hotmail.com>
2023-03-08 10:41:23 +0700

    Tab Navigation

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -22,6 +22,7 @@ import { fetchCampsites } from "../features/campsites/campsitesSlice";
 import { fetchPromotions } from "../features/promotions/promotionsSlice";
 import { fetchComments } from "../features/comments/commentsSlice";
 import LoginScreen from "./LoginScreen";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
 
 const Drawer = createDrawerNavigator();
 
@@ -145,10 +146,16 @@ const LoginNavigator = () => {
             <Stack.Screen
                 name="Login"
                 component={LoginScreen}
-                options={({ navigation }) => ({
+                options={({ navigation, route }) => ({
+                    headerTitle: getFocusedRouteNameFromRoute(route),
                     headerLeft: () => (
                         <Icon
-                            name="sign-in"
+                            name={
+                                getFocusedRouteNameFromRoute(route) === 
+                                "Register"
+                                    ? "user-plus"
+                                    : "sign-in"
+                            }
                             type="font-awesome"
                             iconStyle={styles.stackIcon}
                             onPress={() => navigation.toggleDrawer()}
